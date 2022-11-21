b33ccc0af7f16097215bf4bff778b5f7e1d61e65
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-21 16:33:07 +0300

    Phase 1 completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Rawllings Kiprop"
+// const welcomeMessage = "Welcome to Flatbook, "
+const welcomeMessage = `"Welcome to Flatbook, " ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+console.log (excitedWelcomeMessage);
+console.log (welcomeMessage);
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+// const shortGreeting = `Welcome, ${currentUser.slice(0)}!`
